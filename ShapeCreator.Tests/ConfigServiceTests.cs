b2f5656ab7fd7c0cf25156db10afbf9e458d557d
a.deryagin <aderyagin@gmcs.ru>
2026-07-19 17:40:21 +0300

    nunit for ConfigService and ValidateService

---
+++ ShapeCreator.Tests/ConfigServiceTests.cs
@@ -0,0 +1,51 @@
+﻿using Moq;
+using ShapeCreator.Models;
+using ShapeCreator.Services;
+using ShapeCreator.Services.Interface;
+
+namespace ShapeCreator.Tests
+{
+    [TestFixture]
+    public class ConfigServiceTests
+    {
+        private Mock<ILoggerService> mockLoggerService;
+        private Mock<IFileService> mockFileService;
+        private Mock<IUiService> mockUiService;
+
+        private ConfigService configService;
+
+        [SetUp]
+        public void Setup()
+        {
+            mockLoggerService = new Mock<ILoggerService>();
+            mockFileService = new Mock<IFileService>();
+            mockUiService = new Mock<IUiService>();
+
+            configService = new ConfigService(mockLoggerService.Object, mockFileService.Object, mockUiService.Object);
+        }
+
+        [Test]
+        public void ConfigService_IsValid_Valid_Test()
+        {
+            mockFileService
+                .Setup(x => x.GetRootFromFile(It.IsAny<string>()))
+                .Returns((true, string.Empty, new Root()));
+
+            bool isValid = configService.LoadTestProject();
+
+            Assert.That(isValid, Is.True);
+        }
+
+        [Test]
+        public void ConfigService_IsValid_InValid_Test()
+        {
+            mockFileService
+                .Setup(x => x.GetRootFromFile(It.IsAny<string>()))
+                .Returns((false, "описание ошибки", new Root()));
+
+            bool isValid = configService.LoadTestProject();
+
+            Assert.That(isValid, Is.False);
+        }
+    }
+}
---
+++ ShapeCreator.Tests/ValidateServiceTests.cs
@@ -0,0 +1,118 @@
+﻿using Moq;
+using ShapeCreator.Models;
+using ShapeCreator.Services;
+using ShapeCreator.Services.Interface;
+
+namespace ShapeCreator.Tests
+{
+    [TestFixture]
+    public class ValidateServiceTests
+    {
+        private Mock<ILoggerService> mockLoggerService;
+        private ValidateService validateService;
+
+        [SetUp]
+        public void Setup()
+        {
+            mockLoggerService = new Mock<ILoggerService>();
+            validateService = new ValidateService(mockLoggerService.Object);
+        }
+
+        [Test]
+        public void ValidateService_IsValid_ValidOneShape_Test()
+        {
+            var shape = new Shape()
+            {
+                Name = "name",
+                Id = "1",
+                IsActive = true,
+                ShapeType = ShapeType.Circle,
+                CoordinateStart = new Coordinate() { X = 0, Y = 0 },
+                CoordinateFinish = new Coordinate() { X = 100, Y = 100 },
+            };
+            var shapes = new List<Shape>() { shape };
+
+            (bool isValid, string message) = validateService.IsValid(shapes);
+
+            Assert.That(isValid, Is.True);
+        }
+
+        [Test]
+        public void ValidateService_IsValid_ValidTwoShape_Test()
+        {
+            var shapeOne = new Shape()
+            {
+                Name = "one",
+                Id = "1",
+                IsActive = true,
+                ShapeType = ShapeType.Circle,
+                CoordinateStart = new Coordinate() { X = 150, Y = 100 },
+                CoordinateFinish = new Coordinate() { X = 200, Y = 150 },
+            };
+            var shapeTwo = new Shape()
+            {
+                Name = "two",
+                Id = "2",
+                IsActive = true,
+                ShapeType = ShapeType.Circle,
+                CoordinateStart = new Coordinate() { X = 10, Y = 10 },
+                CoordinateFinish = new Coordinate() { X = 50, Y = 50 },
+            };
+            var shapes = new List<Shape>() { shapeOne, shapeTwo };
+
+            (bool isValid, string message) = validateService.IsValid(shapes);
+
+            Assert.That(isValid, Is.True);
+            Assert.That(message, Is.Empty);
+        }
+
+        [Test]
+        public void ValidateService_IsValid_InValidNegativeСoordinate_Test()
+        {
+            var shape = new Shape()
+            {
+                Name = "name",
+                Id = "1",
+                IsActive = true,
+                ShapeType = ShapeType.Circle,
+                CoordinateStart = new Coordinate() { X = -100, Y = 100 },
+                CoordinateFinish = new Coordinate() { X = 100, Y = 100 },
+            };
+            var shapes = new List<Shape>() { shape };
+
+            (bool isValid, string message) = validateService.IsValid(shapes);
+
+            Assert.That(isValid, Is.False);
+            Assert.That(message, !Is.Empty);
+        }
+
+        [Test]
+        public void ValidateService_IsValid_InValidDuplicateName_Test()
+        {
+            var shapeOne = new Shape()
+            {
+                Name = "name",
+                Id = "1",
+                IsActive = true,
+                ShapeType = ShapeType.Circle,
+                CoordinateStart = new Coordinate() { X = 150, Y = 100 },
+                CoordinateFinish = new Coordinate() { X = 200, Y = 150 },
+            };
+            var shapeTwo = new Shape()
+            {
+                Name = "name",
+                Id = "2",
+                IsActive = true,
+                ShapeType = ShapeType.Circle,
+                CoordinateStart = new Coordinate() { X = 10, Y = 10 },
+                CoordinateFinish = new Coordinate() { X = 50, Y = 50 },
+            };
+            var shapes = new List<Shape>() { shapeOne, shapeTwo };
+
+            (bool isValid, string message) = validateService.IsValid(shapes);
+
+            Assert.That(isValid, Is.False);
+            Assert.That(message, !Is.Empty);
+        }
+    }
+}
